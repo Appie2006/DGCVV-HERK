30eefd66448a0b8ceca47fc88ea5bad6be14cfcd
Appie2006 <159128361+Appie2006@users.noreply.github.com>
2026-06-17 15:33:32 +0200

    us05
    US 05 - Springmechanisme met ground-check geïmplementeerd

--- De Gouden CD van Verkade/Assets/_Scripts/PlayerMovement.cs
+++ De Gouden CD van Verkade/Assets/_Scripts/PlayerMovement.cs
@@ -2,27 +2,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Snelheid van de speler (aanpasbaar in de Unity Inspector)
     public float moveSpeed = 8f; 
+    
+    // NIEUW: De kracht van de sprong (aanpasbaar in Unity Inspector)
+    public float jumpForce = 12f; 
     
     private Rigidbody2D rb;
     private float horizontalInput;
+    
+    // NIEUW: Een ja/nee variabele om bij te houden of de speler op de grond staat
+    private bool isGrounded; 
 
     void Start()
     {
-        // Haal de Rigidbody2D component op die op de speler staat
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        // Vang de input op. GetAxisRaw geeft direct -1 (A/Links) of 1 (D/Rechts)
         horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        // NIEUW: Als de speler op Spatie drukt EN op de grond staat, dan springen!
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            // We behouden de horizontale snelheid (x), maar geven een verticale boost omhoog (jumpForce)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            
+            // Zodra we springen, staan we niet meer op de grond
+            isGrounded = false; 
+        }
     }
 
     void FixedUpdate()
     {
-        // Beweeg de speler over de X-as. De Y-as laten we met rust, zodat de zwaartekracht zijn werk blijft doen!
         rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    // NIEUW: Unity detecteert automatisch wanneer de speler ergens op landt
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // We controleren of het object waar we op landen "Ground" heet
+        if (collision.gameObject.name == "Ground")
+        {
+            isGrounded = true;
+        }
     }
 }
